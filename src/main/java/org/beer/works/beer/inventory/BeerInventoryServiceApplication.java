27a8c0ecf93475243e82d197ecd957728ac26ff0
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-02 16:25:06 -0600

    Init commit

---
+++ src/main/java/org/beer/works/beer/inventory/BeerInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.beer.works.beer.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/beer/works/beer/inventory/BeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.beer.works.beer.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
