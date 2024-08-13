f15f93491bda74deb34359b1a7d2cbd7f595f3d1
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-12 23:22:56 -0600

    Add missing classes

---
+++ src/main/java/org/beer/works/common/events/BeerDto.java
@@ -0,0 +1,54 @@
+package org.beer.works.common.events;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable {
+
+    static final long serialVersionUID = 2456226104051694066L;
+
+    @Null
+    private UUID id;
+
+    @Null
+    private Integer version;
+
+    @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    private OffsetDateTime createdDate;
+
+    @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    private String beerName;
+
+    @NotNull
+    private BeerStyleEnum beerStyle;
+
+    @NotNull
+    private String upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Positive
+    @NotNull
+    private BigDecimal price;
+    private Integer qualityOnHand;
+}
---
+++ src/main/java/org/beer/works/common/events/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package org.beer.works.common.events;
+
+public enum BeerStyleEnum {
+    LAGER, PISLSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
